39a0a30eb5dea5dd11251d4657c0ae28a409e23a
RABBIT <rabbitgit90@mail.ru>
2019-10-03 14:28:33 +0300

    1) API запрос апп-бар

--- app/modules/v1/controllers/RefBlankSexController.php
+++ app/modules/v1/controllers/RefBlankSexController.php
@@ -14,6 +14,7 @@ use app\modules\v1\models\ref\RefBlankClass;
 use app\modules\v1\models\ref\RefBlankGroup;
 use app\modules\v1\models\ref\RefBlankModel;
 use app\modules\v1\models\ref\RefBlankSex;
+use yii\db\Query;
 
 class RefBlankSexController extends ActiveControllerExtended
 {
@@ -37,10 +38,14 @@ class RefBlankSexController extends ActiveControllerExtended
         $sexes = ['man' => [1, 5], 'woman' => [2, 5], 'kids' => [3]];
 
         foreach ($sexes as $sex => $sexIds) {
-            $resp[$sex] = RefBlankGroup::find()
-                ->joinWith(['refBlankClasses', 'refBlankClasses.refBlankModels', 'refBlankClasses.refBlankModels.sexFk'])
-                ->where(['{{ref_blank_sex}}.id' => $sexIds])
-                ->asArray()
+            $resp[$sex] = (new Query())
+                ->select('ref_blank_class.tag AS classTitle, ref_blank_class.id AS classId, ref_blank_group.title AS groupTitle, ref_blank_group.id AS groupId')
+                ->distinct()
+                ->from('ref_blank_model')
+                ->leftJoin('ref_blank_class', 'ref_blank_model.class_fk = ref_blank_class.id')
+                ->leftJoin('ref_blank_group', 'ref_blank_class.group_fk = ref_blank_group.id')
+                ->leftJoin('ref_blank_sex', 'ref_blank_model.sex_fk = ref_blank_sex.id')
+                ->where(['ref_blank_sex.id' => $sexIds])
                 ->all();
         }
 
