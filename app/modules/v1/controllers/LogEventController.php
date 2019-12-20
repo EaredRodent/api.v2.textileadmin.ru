61a9787f902785ed46483ae9bfb83792c8b03e28
alexander <alexander.a.rud@gmail.com>
2019-12-20 11:23:18 +0300

    1) отчет по продажам

--- app/modules/v1/controllers/LogEventController.php
+++ app/modules/v1/controllers/LogEventController.php
@@ -67,12 +67,18 @@ class LogEventController extends ActiveControllerExtended
      */
     function actionLogBrowser()
     {
-        $logEvent = new LogEvent();
-        $logEvent->event = 'LogOutdatedBrowser';
-        $logEvent->params = json_encode([
+        $params = json_encode([
             'userIP' => Yii::$app->request->userIP,
             'userAgent' => Yii::$app->request->userAgent
         ], JSON_UNESCAPED_UNICODE);
-        $logEvent->save();
+
+        $logEvent = LogEvent::findOne(['event' => 'LogOutdatedBrowser', 'params' => $params]);
+
+        if(!$logEvent) {
+            $logEvent = new LogEvent();
+            $logEvent->event = 'LogOutdatedBrowser';
+            $logEvent->params = $params;
+            $logEvent->save();
+        }
     }
 }
